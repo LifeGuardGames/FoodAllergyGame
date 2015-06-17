d357ccf1ddce98e4552d54cac7472de3d16cddc0
Dylan <dylan@lifeguardgames.com>
2015-06-17 17:23:16 -0400

    demo stuff

---
+++ FoodAllergyGame/Assets/Scripts/TempSceneChanger.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TempSceneChanger : MonoBehaviour {
+
+	public void ResetGame(){
+		Application.LoadLevel(SceneUtils.MENUPLANNING);
+	}
+}
