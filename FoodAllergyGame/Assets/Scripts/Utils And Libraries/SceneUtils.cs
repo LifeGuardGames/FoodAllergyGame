f8e90c494ace38f66a1ae4f6b8aa2fa13f8b0a0f
Wenshiang Chung <aliken9@gmail.com>
2015-06-18 13:13:18 -0400

    Start scene

--- FoodAllergyGame/Assets/Scripts/Utils And Libraries/SceneUtils.cs
+++ FoodAllergyGame/Assets/Scripts/Utils And Libraries/SceneUtils.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 
 public static class SceneUtils{
+	public static string START = "StartScene";
 	public static string MENUPLANNING = "MenuPlanning";
 	public static string RESTAURANT = "Restaurant";
 }
---
+++ FoodAllergyGame/Assets/Scripts/_Start/StartManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartManager : Singleton<StartManager>{
+
+	void Start(){
+		//TODO Generate event from data
+
+
+		//TODO Set up visuals and appearances for that day
+
+
+	}
+
+	public void OnPlayButtonClicked(){
+		TransitionManager.Instance.TransitionScene(SceneUtils.MENUPLANNING);
+	}
+
+	public void OnDecoButtonClicked(){
+
+	}
+
+	public void OnInfoByttonClicked(){
+
+	}
+}
