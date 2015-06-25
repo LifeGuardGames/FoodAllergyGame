0e3eb87a60efa0e57ead362e12ed1eb6ce28431a
Dylan <dylan@lifeguardgames.com>
2015-06-25 17:45:49 -0400

    2 new customers have been added to the rotation

---
+++ FoodAllergyGame/Assets/Scripts/Customers/CustomerHospitalRunner.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CustomerHospitalRunner : Customer {
+
+	public override void AllergyAttack ()
+	{
+		satisfaction = -20;
+		customerUI.UpdateSatisfaction(satisfaction);
+		customerAnim.SetSatisfaction(satisfaction);
+		
+		if(order.gameObject != null){
+			Destroy(order.gameObject);
+		}
+		NotifyLeave();
+	}
+}
