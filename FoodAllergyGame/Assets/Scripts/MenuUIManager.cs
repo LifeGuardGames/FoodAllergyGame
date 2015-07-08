2371c7b6c247c76c1da6b0f9c944fece8ff7ed96
Wenshiang Chung <aliken9@gmail.com>
2015-07-08 17:18:32 -0400

    Made ordering food more obvious

--- FoodAllergyGame/Assets/Scripts/MenuUIManager.cs
+++ FoodAllergyGame/Assets/Scripts/MenuUIManager.cs
@@ -9,15 +9,13 @@ public class MenuUIManager : MonoBehaviour {
 	public Image button1Image;
 	public GameObject button2;
 	public Image button2Image;
-	public GameObject button3;
-	public Image button3Image;
 	public List <ImmutableDataFood> choices;
 	public int tableNum;
 	public GameObject order;
+	public TweenToggle menuTweenToggle;
 
 	void Start(){
 		choices = new List<ImmutableDataFood>();
-		//order = new GameObject ();
 	}
 
 	public void ShowChoices(List <ImmutableDataFood> foodChoices, int table){
@@ -26,23 +24,19 @@ public class MenuUIManager : MonoBehaviour {
 //		Debug.Log (foodChoices[0].ID.ToString());
 		button1Image.sprite = SpriteCacheManager.Instance.GetFoodSpriteData(foodChoices[0].SpriteName);
 		button2Image.sprite = SpriteCacheManager.Instance.GetFoodSpriteData(foodChoices[1].SpriteName);
-		//button3.GetComponent<Image>().sprite = Resources.Load(foodChoices[2].SpriteName)as Sprite;
-		button1.SetActive(true);
-		button2.SetActive(true);
-		//button3.SetActive(true);
-
+		
+		menuTweenToggle.Show();
 	}
 
 	public void CancelOrder(int table){
 		if(table == tableNum){
-			button1.SetActive(false);
-			button2.SetActive(false);
+			menuTweenToggle.Hide();
 		}
 	}
 
 	public void ProduceOrder(int choice){
-		button1.SetActive(false);
-		button2.SetActive(false);
+		menuTweenToggle.Hide();
+
 		//if(Waiter.Instance.CheckHands()){
 		//Waiter.Instance.WriteDownOrder(orderObj);
 		//}
