f3674cb684010cbbca4baf871da65b10b3c25a2c
Wenshiang Chung <aliken9@gmail.com>
2015-07-10 17:29:01 -0400

    Integrated comic with start menu

--- FoodAllergyGame/Assets/Scripts/ComicManager.cs
+++ FoodAllergyGame/Assets/Scripts/ComicManager.cs
@@ -17,21 +17,18 @@ public class ComicManager : MonoBehaviour {
 			currScene = Resources.Load("ComicPage1")as GameObject;
 			comicPage = GameObjectUtils.AddChildGUI(canvas, currScene);
 			comicPage.GetComponentInChildren<Button>().onClick.AddListener(() => ComicStep(2));
-			AudioManager.Instance.PlayClip("customerEnter");
 			break;
 		case 2:
 			Destroy(comicPage);
 			currScene = Resources.Load("ComicPage2")as GameObject;
 			comicPage = GameObjectUtils.AddChildGUI(canvas, currScene);
 			comicPage.GetComponentInChildren<Button>().onClick.AddListener(() => ComicStep(3));
-			AudioManager.Instance.PlayClip("pop");
 			break;
 		case 3:
 			Destroy(comicPage);
 			currScene = Resources.Load("ComicPage3")as GameObject;
 			comicPage = GameObjectUtils.AddChildGUI(canvas, currScene);
 			comicPage.GetComponentInChildren<Button>().onClick.AddListener(() => ComicStep(4));
-			AudioManager.Instance.PlayClip("readingMenu");
 			break;
 		case 4:
 			Application.LoadLevel(SceneUtils.START);
