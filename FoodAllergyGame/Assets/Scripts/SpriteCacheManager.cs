9bb3bf5b274401cee8c2d5ee07ffafde684279f1
Wenshiang Chung <aliken9@gmail.com>
2015-07-14 11:28:59 -0400

    Images fix 1

--- FoodAllergyGame/Assets/Scripts/SpriteCacheManager.cs
+++ FoodAllergyGame/Assets/Scripts/SpriteCacheManager.cs
@@ -7,22 +7,27 @@ using System.Collections.Generic;
 // NOTE: this is a sprite list call-driven population,
 //		so it will miss the food if there is no sprite
 public class SpriteCacheManager : Singleton<SpriteCacheManager> {
-	public Dictionary<string, Sprite> foodSpriteDictionary = new Dictionary<string, Sprite>();
+
+//	public Dictionary<string, Sprite> foodSpriteDictionary = new Dictionary<string, Sprite>();
+//	public Sprite GetFoodSpriteData(string spriteName){
+//		if(foodSpriteDictionary.Count == 0){
+//			// Set up sprite list from data
+//			Sprite[] sprites = Resources.LoadAll<Sprite>("food");
+//			foreach(Sprite spr in sprites){
+//				foodSpriteDictionary.Add(spr.name, spr);	// Adding all sprites for now
+//			}
+//		}
+//
+//		if(!foodSpriteDictionary.ContainsKey(spriteName)){
+//			Debug.LogError("Can not find key : " + spriteName);
+//		}
+//
+//		return foodSpriteDictionary[spriteName];
+//	}
 
 	public Sprite GetFoodSpriteData(string spriteName){
-		if(foodSpriteDictionary.Count == 0){
-			// Set up sprite list from data
-			Sprite[] sprites = Resources.LoadAll<Sprite>("food");
-			foreach(Sprite spr in sprites){
-				foodSpriteDictionary.Add(spr.name, spr);	// Adding all sprites for now
-			}
-		}
-
-		if(!foodSpriteDictionary.ContainsKey(spriteName)){
-			Debug.LogError("Can not find key : " + spriteName);
-		}
-
-		return foodSpriteDictionary[spriteName];
+		Sprite sprite = Resources.Load<Sprite>(spriteName);
+		return sprite;
 	}
 
 	public Sprite GetCustomerSpriteData(string spriteName){
