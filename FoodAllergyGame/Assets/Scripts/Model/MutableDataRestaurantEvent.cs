4ed08036b3618b5daffd3c72bdc533035dc57a85
Wenshiang Chung <aliken9@gmail.com>
2015-07-17 13:03:31 -0400

    added menu planning stock

--- FoodAllergyGame/Assets/Scripts/Model/MutableDataRestaurantEvent.cs
+++ FoodAllergyGame/Assets/Scripts/Model/MutableDataRestaurantEvent.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
 
 public class MutableDataRestaurantEvent{
 
 	public string CurrentEvent {get; set;}
+	public List<string> menuPlanningStock {get; set;}	// The final food stock that will go to populate menu planning scene
 
 	public MutableDataRestaurantEvent(){
 		CurrentEvent = "Event00";
+		menuPlanningStock = new List<string>();
 	}
 }
