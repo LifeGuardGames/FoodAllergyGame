23ee6642df51f12b7abbaeb8035c5cd2634c3ec6
Wenshiang Chung <aliken9@gmail.com>
2015-07-29 16:34:43 -0400

    New food spinner inside

--- FoodAllergyGame/Assets/Scripts/RestaurantManager.cs
+++ FoodAllergyGame/Assets/Scripts/RestaurantManager.cs
@@ -210,8 +210,8 @@ public class RestaurantManager : Singleton<RestaurantManager>{
 		return menuUIController;
 	}
 
-	public AllergyChartUIController GetAllergyUIController(){
-		return allergyChartUIController;
+	public void ShowAllergyChart(){
+		allergyChartUIController.OnOpenButton();
 	}
 
 	public KitchenManager GetKitchen(){
