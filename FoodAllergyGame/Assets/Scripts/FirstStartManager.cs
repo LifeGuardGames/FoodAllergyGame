1e31563cec0a27c07ff7ab81daaf4e705492bd37
Wenshiang Chung <aliken9@gmail.com>
2015-08-05 18:05:44 -0400

    Intro scene leading to comic scene

---
+++ FoodAllergyGame/Assets/Scripts/FirstStartManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FirstStartManager : Singleton<FirstStartManager> {
+
+	public Animation restaurantAnimation;
+	public GameObject button;
+
+	public void ButtonClicked(){
+		button.SetActive(false);
+		restaurantAnimation.Play();
+	}
+
+	public void FinishedAnimation(){
+		TransitionManager.Instance.TransitionScene(SceneUtils.COMICSCENE);
+	}
+}
