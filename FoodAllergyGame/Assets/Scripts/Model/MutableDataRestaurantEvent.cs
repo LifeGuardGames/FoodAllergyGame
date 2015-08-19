1c874fe0161a8e72e7d46a54719511276a7ce4c9
Wenshiang Chung <aliken9@gmail.com>
2015-08-19 14:50:20 -0400

    Forgot to remove data

--- FoodAllergyGame/Assets/Scripts/Model/MutableDataRestaurantEvent.cs
+++ FoodAllergyGame/Assets/Scripts/Model/MutableDataRestaurantEvent.cs
@@ -4,12 +4,10 @@ using System.Collections.Generic;
 public class MutableDataRestaurantEvent{
 
 	public string CurrentEvent {get; set;}
-	public List<string> MenuPlanningStock {get; set;}	// The final food stock that will go to populate menu planning scene
 	public bool ShouldGenerateNewEvent {get; set;}
 
 	public MutableDataRestaurantEvent(){
 		CurrentEvent = "EventT1";
-		MenuPlanningStock = new List<string>();
 		ShouldGenerateNewEvent = true;
 	}
 }
