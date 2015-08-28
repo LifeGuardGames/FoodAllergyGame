50fb05457ffb45e94f7a736603a65bd6c804cd99
aliken9 <aliken9@gmail.com>
2015-08-28 12:00:27 -0400

    New button controller and food button

---
+++ FoodAllergyGame/Assets/Scripts/_Start/FoodInfoButton.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class FoodInfoButton : MonoBehaviour {
+	public string foodID;
+	public Image image;
+	public Text label;
+	public Button button;
+	
+	public void Init(ImmutableDataFood foodData){
+		foodID = foodData.ID;
+		gameObject.name = foodData.ID;
+		label.text = LocalizationText.GetText(foodData.FoodNameKey);
+		image.sprite = SpriteCacheManager.Instance.GetFoodSpriteData(foodData.SpriteName);
+	}
+	
+	public void OnButtonClick(){
+		if(string.Equals(Application.loadedLevelName, SceneUtils.START)){
+			InfoManager.Instance.ShowDetail(InfoType.Customer, foodID);
+		}
+	}
+}
