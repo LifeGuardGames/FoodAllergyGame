4c8ac4afffc7a202ce596eb9afecb3f51820f3e1
Dylan <dylan@lifeguardgames.com>
2015-09-22 12:56:37 -0400

    deco tut WIP

---
+++ FoodAllergyGame/Assets/Scripts/Model/DataLoaderDecoTut.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DataLoaderDecoTut : XMLLoaderGeneric<DataLoaderDecoTut>{
+
+public static ImmutableDataFood GetData(string id){
+		instance.InitXMLLoader();
+		return instance.GetData<ImmutableDataDecoTut>(id);
+	}
+	
+	public static List<ImmutableDataDecoTut> GetDataList(){
+		instance.InitXMLLoader();
+		return instance.GetDataList<ImmutableDataDecoTut>();
+	}
+	
+	protected override void XMLNodeHandler(string id, IXMLNode xmlNode, Hashtable hashData, string errorMessage){
+		ImmutableDataDecoTut data = new ImmutableDataDecoTut(id, xmlNode, errorMessage);
+		// Store the data
+		if(hashData.ContainsKey(id))
+			Debug.LogError(errorMessage + "Duplicate keys!");
+		else
+			hashData.Add(id, data);
+	}
+	
+	protected override void InitXMLLoader(){
+		xmlFileFolderPath = "DecoTut";
+	}
+}
---
+++ FoodAllergyGame/Assets/Scripts/Model/ImmutableDataDecoTut.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class ImmutableDataDecoTut{
+
+	private string id;
+	public string ID{
+		get{ return id; }
+	}
+
+	private string image;
+	public string Image{
+		get{return image;}
+	}
+
+	private string text;
+	public string Text{
+		get { return text;}
+	}
+
+	public ImmutableDataDecoTut(string id, IXMLNode xmlNode, string error){
+		Hashtable hashElements = XMLUtils.GetChildren(xmlNode);
+		
+		this.id = id;
+		image = XMLUtils.GetString(hashElements["Image"] as IXMLNode, null, error);
+		text = XMLUtils.GetFloat(hashElements["Text"] as IXMLNode);
+	}
+}
