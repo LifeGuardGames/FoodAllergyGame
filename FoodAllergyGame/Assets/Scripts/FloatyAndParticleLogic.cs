d3c9955871a36cbf2419a523cfaef56b0a18372a
Dylan <dylan@lifeguardgames.com>
2015-09-30 18:37:18 -0400

    finished bought deco

--- FoodAllergyGame/Assets/Scripts/FloatyAndParticleLogic.cs
+++ FoodAllergyGame/Assets/Scripts/FloatyAndParticleLogic.cs
@@ -8,7 +8,7 @@ public class FloatyAndParticleLogic : MonoBehaviour {
 		transform.SetParent(GameObject.Find ("Canvas").transform);
 		RectTransform UI_Element = this.GetComponent<RectTransform>();
 		RectTransform CanvasRect = GameObject.Find ("Canvas").GetComponent<RectTransform>();
-		Vector2 ViewportPosition=Camera.main.WorldToViewportPoint(this.transform.position);
+		Vector2 ViewportPosition=Camera.main.ScreenToViewportPoint(this.transform.position);
 		Vector2 WorldObject_ScreenPosition=new Vector2(
 			((ViewportPosition.x*CanvasRect.sizeDelta.x)-(CanvasRect.sizeDelta.x*0.5f)),
 			((ViewportPosition.y*CanvasRect.sizeDelta.y)-(CanvasRect.sizeDelta.y*0.5f)));
