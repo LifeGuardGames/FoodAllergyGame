60ba23ffd0a6cab2a3f4e1fd2d56deeead85e68d
Dylan <dylan@lifeguardgames.com>
2015-09-30 11:37:08 -0400

    fixed warnings

--- FoodAllergyGame/Assets/Scripts/Utils And Libraries/ParticleUtils.cs
+++ FoodAllergyGame/Assets/Scripts/Utils And Libraries/ParticleUtils.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using UnityEngine.UI;
 
 public static class ParticleUtils{
+
 	static public void  PlayMoneyFloaty(Vector3 pos, int amount){
 		GameObject go = Resources.Load("CoinFloaty") as GameObject;
 		GameObject Instance = GameObject.Instantiate(go, pos, go.transform.rotation) as GameObject;
@@ -17,17 +18,17 @@ public static class ParticleUtils{
 
 	static public void  PlayDecoChangePoof(Vector3 pos){
 		GameObject go = Resources.Load("DecoPoof") as GameObject;
-		GameObject Instance = GameObject.Instantiate(go, pos, go.transform.rotation) as GameObject;
+		GameObject.Instantiate(go, pos, go.transform.rotation);
 	}
 
 	static public void  PlayTeleportParticle(Vector3 pos){
 		GameObject go = Resources.Load("ParticleTeleport") as GameObject;
-		GameObject Instance = GameObject.Instantiate(go, pos, go.transform.rotation) as GameObject;
+		GameObject.Instantiate(go, pos, go.transform.rotation);
 	}
 
 	static public void  PlayHandsFullFloaty(Vector3 pos){
 		GameObject go = Resources.Load("HandsFull") as GameObject;
-		GameObject Instance = GameObject.Instantiate(go, pos, go.transform.rotation) as GameObject;
+		GameObject.Instantiate(go, pos, go.transform.rotation);
 		AudioManager.Instance.PlayClip("HandsFull");
 	}
 }
