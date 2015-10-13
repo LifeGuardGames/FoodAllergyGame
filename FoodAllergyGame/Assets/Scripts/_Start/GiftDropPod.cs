427a6d9f74ee297cba1b92317ba7e12f10970c8c
aliken9 <aliken9@gmail.com>
2015-10-13 13:15:00 -0400

    Orbital drop pod for gifts

---
+++ FoodAllergyGame/Assets/Scripts/_Start/GiftDropPod.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GiftDropPod : MonoBehaviour {
+	public SpriteRenderer podSprite;
+	public Sprite crashSpriteResource;
+
+	// Animation event
+	public void Impact(){
+		Camera.main.GetComponent<Animation>().Play();
+		podSprite.sprite = crashSpriteResource;
+	}
+}
