5e48c7059510a5674499f6b1bba62f732a6ca8d8
Dylan <dylan@lifeguardgames.com>
2015-11-04 14:22:30 -0500

    deco tabs now turn off if they cannot be used

---
+++ FoodAllergyGame/Assets/Scripts/DecoTabController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DecoTabController : MonoBehaviour {
+	public DecoTypes deco;
+	// Use this for initialization
+	void Start () {
+		if(!TierManager.Instance.IsCategoryUnlocked(deco)) {
+			this.gameObject.SetActive(false);
+		}
+	}
+}
