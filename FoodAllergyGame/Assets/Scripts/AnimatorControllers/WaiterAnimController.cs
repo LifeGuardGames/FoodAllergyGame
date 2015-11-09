264e99276d6c1144fe8d700bf191ea6970f7ea9e
aliken9 <aliken9@gmail.com>
2015-11-09 18:43:39 -0500

    Waiter animation 2 integration WIP

--- FoodAllergyGame/Assets/Scripts/AnimatorControllers/WaiterAnimController.cs
+++ FoodAllergyGame/Assets/Scripts/AnimatorControllers/WaiterAnimController.cs
@@ -1,52 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/// <summary>
+/// Custom animation controller for the waiter
+/// Note: Make sure this is attached to the skeleton itself, it does a x-scale for flipping
+/// So putting it in the base parent gameobject wont work
+/// </summary>
 public class WaiterAnimController : MonoBehaviour {
 	public SkeletonAnimation skeleton;
-	private bool isCurrentlyMoving = false;
+	private bool isMoving = false;
 	private bool isFacingRight = true;
 	private WaiterHands hand1 = WaiterHands.None;	// Aux that is synced with waiter
 	private WaiterHands hand2 = WaiterHands.None;
 	private WaiterHands lastHand1 = WaiterHands.None;   // Aux that is synced with waiter
 	private WaiterHands lastHand2 = WaiterHands.None;
+	private string currentBodyAnimation = "";
 
 	public void ChangeOrderInLayer(int order) {
 		// TODO Finish this!!!
 	}
 
-	// TODO Rip out separate animations
-	void Start() {
-		Reset();
-		 skeleton.state.SetAnimation(0, "Blink", true);
-		//skeleton.state.SetAnimation(0, "ArmRunBack", true);
-		//skeleton.state.SetAnimation(1, "Sprint", true);
-		//FlipAndSetHand();
-	}
-
 	private void Reset() {
-		skeleton.state.ClearTracks();
-	//	skeleton.state.SetAnimation(0, "Reset", false);
+		//skeleton.state.ClearTracks();
 	}
 
 	public void SetMoving(bool isMoving) {
-		if(isMoving) {
-			if(!isCurrentlyMoving) {
-				isCurrentlyMoving = true;
-				skeleton.state.ClearTrack(1);
-				skeleton.state.SetAnimation(1, "Sprint", true);
-            }
-		}
-		else {
-			isCurrentlyMoving = false;
-			skeleton.state.ClearTrack(1);
-			skeleton.state.SetAnimation(1, "Idle", true);
-		}
-	}
+		this.isMoving = isMoving;
+		FlipAndSetHand();
+    }
 
 	public void SetDirection(bool isFacingRight) {
         transform.localScale = new Vector3(isFacingRight ? 1f : -1f, 1f, 1f);
 		this.isFacingRight = isFacingRight;
-		//FlipAndSetHand();
+		FlipAndSetHand();
     }
 	
 	public void RefreshHand() {
@@ -56,36 +42,48 @@ public class WaiterAnimController : MonoBehaviour {
     }
 	
 	private void FlipAndSetHand() {
-		/*
-		// hand1 has track 2 and hand2 has track 3
-		if(isCurrentlyMoving) {
-			if(isFacingRight) {
-				//skeleton.state.ClearTrack(2);
-				skeleton.state.AddAnimation(2, hand1 == WaiterHands.None ? "ArmRunBack" : "ArmRunCarryBack", true, 0f);
-				//skeleton.state.ClearTrack(3);
-				skeleton.state.AddAnimation(3, hand2 == WaiterHands.None ? "ArmRunFront" : "ArmRunCarryFront", true, 0f);
+		if(isMoving) {
+			if(hand1 == WaiterHands.None && hand1 == WaiterHands.None) {
+				SetBodyAnimation("Run");
+            }
+			else if(hand1 != WaiterHands.None && hand1 == WaiterHands.None) {
+				SetBodyAnimation(isFacingRight ? "RunCarryBack" : "RunCarryFront");
 			}
-			else {	// Switch the front and back arm animations
-				//skeleton.state.ClearTrack(2);
-				skeleton.state.SetAnimation(2, hand1 == WaiterHands.None ? "ArmRunFront" : "ArmRunCarryFront", true);
-				//skeleton.state.ClearTrack(3);
-				skeleton.state.SetAnimation(3, hand2 == WaiterHands.None ? "ArmRunBack" : "ArmRunCarryBack", true);
+			else if(hand1 == WaiterHands.None && hand1 != WaiterHands.None) {
+				SetBodyAnimation(isFacingRight ? "RunCarryFront" : "RunCarryBack");
+			}
+			else if(hand1 != WaiterHands.None && hand1 != WaiterHands.None) {
+				SetBodyAnimation("RunCarryBoth");
+			}
+			else {
+				Debug.LogError("Bad waiter state");
 			}
 		}
 		else {
-			if(isFacingRight) {
-				//skeleton.state.ClearTrack(2);
-				skeleton.state.AddAnimation(2, hand1 == WaiterHands.None ? "ArmIdleBack" : "ArmIdleCarryBack", true, 0f);
-				//skeleton.state.ClearTrack(3);
-				skeleton.state.AddAnimation(3, hand2 == WaiterHands.None ? "ArmIdleFront" : "ArmIdleCarryFront", true, 0f);
+			if(hand1 == WaiterHands.None && hand1 == WaiterHands.None) {
+				SetBodyAnimation("Idle");
 			}
-			else {  // Switch the front and back arm animations
-				//skeleton.state.ClearTrack(2);
-				skeleton.state.SetAnimation(2, hand1 == WaiterHands.None ? "ArmIdleFront" : "ArmIdleCarryFront", true);
-				//skeleton.state.ClearTrack(3);
-				skeleton.state.SetAnimation(3, hand2 == WaiterHands.None ? "ArmIdleBack" : "ArmIdleCarryBack", true);
+			else if(hand1 != WaiterHands.None && hand1 == WaiterHands.None) {
+				SetBodyAnimation(isFacingRight ? "IdleCarryBack" : "IdleCarryFront");
+			}
+			else if(hand1 == WaiterHands.None && hand1 != WaiterHands.None) {
+				SetBodyAnimation(isFacingRight ? "IdleCarryFront" : "IdleCarryBack");
+			}
+			else if(hand1 != WaiterHands.None && hand1 != WaiterHands.None) {
+				SetBodyAnimation("IdleCarryBoth");
+			}
+			else {
+				Debug.LogError("Bad waiter state");
 			}
 		}
-		*/
+	}
+
+	private void SetBodyAnimation(string bodyAnimation) {
+		if(currentBodyAnimation != bodyAnimation) {
+			Debug.Log("SETTING " + bodyAnimation);
+			currentBodyAnimation = bodyAnimation;
+			skeleton.skeleton.SetToSetupPose();
+			skeleton.state.SetAnimation(0, bodyAnimation, true);
+		}
 	}
 }
