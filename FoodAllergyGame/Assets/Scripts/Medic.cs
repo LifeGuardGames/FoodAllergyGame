2b873fedbf2da06d98d444e6d3e59c9549714de4
Dylan <dylan@lifeguardgames.com>
2015-11-13 13:17:05 -0500

    extended medic

--- FoodAllergyGame/Assets/Scripts/Medic.cs
+++ FoodAllergyGame/Assets/Scripts/Medic.cs
@@ -57,7 +57,7 @@ public class Medic : Singleton<Medic> {
 	}
 
 	private IEnumerator TreatCustomer(){
-		yield return new WaitForSeconds(1.0f);
+		yield return new WaitForSeconds(3.0f);
 
 		// Saved the person in time
 		if(RestaurantManager.Instance.SickCustomers.Count > 0){
