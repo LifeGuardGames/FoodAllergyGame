7f0d833ee99bdfd07df5cdf0737b823efc7a03c7
aliken9 <aliken9@gmail.com>
2015-11-13 13:43:44 -0500

    Oreos with waiter fix

--- FoodAllergyGame/Assets/Scripts/Model/DataLoaderFood.cs
+++ FoodAllergyGame/Assets/Scripts/Model/DataLoaderFood.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 public class DataLoaderFood : XMLLoaderGeneric<DataLoaderFood> {
 
 	public static ImmutableDataFood GetData(string id){
+		Debug.Log(id);
 		instance.InitXMLLoader();
 		return instance.GetData<ImmutableDataFood>(id);
 	}
