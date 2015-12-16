4522fa4a3241c7369e832cbfaee0faa3db2aea3e
aliken9 <aliken9@gmail.com>
2015-12-16 13:18:30 -0500

    First random prop in

--- FoodAllergyGame/Assets/Scripts/PropTween.cs
+++ FoodAllergyGame/Assets/Scripts/PropTween.cs
@@ -1,9 +1,23 @@
 ﻿using UnityEngine;
 
 public class PropTween : MonoBehaviour {
-	
-	
+
+	public Vector3 finalPosition;
+	public float tweenTime = 1;
+	public LeanTweenType ease = LeanTweenType.linear;
+
+	public GameObject propParentToDestroy;
+	//public GameObject onCompleteObject;
+	//public string onCompleteFunction;
+
 	void Start() {
-		//LeanTween.moveLocalX();
+		LeanTween.moveLocal(gameObject, finalPosition, tweenTime).setEase(ease).setOnComplete(OnTweenComplete);
+	}
+
+	private void OnTweenComplete() {
+		Destroy(propParentToDestroy);
+		//if(onCompleteObject != null) {
+		//	onCompleteObject.SendMessage(onCompleteFunction);
+		//}
 	}
 }
