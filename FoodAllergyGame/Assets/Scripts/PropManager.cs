8d5adc23538ffc40352edff68a8c0111c87feed6
aliken9 <aliken9@gmail.com>
2015-12-17 17:12:50 -0500

    New mechanism for random props

--- FoodAllergyGame/Assets/Scripts/PropManager.cs
+++ FoodAllergyGame/Assets/Scripts/PropManager.cs
@@ -2,17 +2,8 @@
 using System.Collections;
 
 public class PropManager : Singleton<PropManager> {
-
 	public int minimumSpawnTier = 0;
-
-	public GameObject propRandomLeft;
-	public GameObject propRandomRight;
-
-	public GameObject propEventLeft;
-	public GameObject propEventRight;
-
-	public GameObject propGrowthLeft;
-	public GameObject propGrowthRight;
+	public GameObject propRandomOrbit;	// Random props are space props that orbit (rotation) the planetoid
 
 	public void InitProps() {
 		if(TierManager.Instance.Tier >= minimumSpawnTier) {
@@ -27,14 +18,12 @@ public class PropManager : Singleton<PropManager> {
 
 		// Choose between right or left node spawning
 		float xScale = 1f;
-		GameObject spawnLocation = propRandomLeft;
 		if(UnityEngine.Random.Range(0, 2) == 1) {
 			// Left direction
 			xScale = -1f;
-			spawnLocation = propRandomRight;
 		}
 		GameObject prefab = Resources.Load(propRandom.PrefabName) as GameObject;
-		GameObject go =  GameObjectUtils.AddChildWithPosition(spawnLocation, prefab);
+		GameObject go =  GameObjectUtils.AddChildWithPosition(propRandomOrbit, prefab);
 		go.transform.localScale = new Vector3(xScale, 1f, 1f);
 	}
 
---
+++ FoodAllergyGame/Assets/Scripts/PropTweenRotate.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PropTweenRotate : MonoBehaviour {
+
+	public float initEulerAngle;	// Init position can be different from the start position
+	public float endEulerAngle;
+	public float firstTweenTime = 1;
+	public float loopTweenTime = 1;
+	public LeanTweenType ease = LeanTweenType.linear;
+
+	public bool isLoop = false;
+	public GameObject propParentToDestroy;
+
+	private float tweenTimeAux;
+	private float eulerAngleDifference;
+
+	void Start() {
+		tweenTimeAux = firstTweenTime;
+		eulerAngleDifference = endEulerAngle - transform.localEulerAngles.z;
+		TweenAction();
+	}
+
+	private void TweenAction() {
+		LeanTween.cancel(gameObject);
+		LeanTween.rotateAroundLocal(gameObject, Vector3.forward, eulerAngleDifference, tweenTimeAux)
+			.setEase(ease)
+			.setOnComplete(OnTweenComplete);
+	}
+
+	private void OnTweenComplete() {
+		if(isLoop) {
+			StartCoroutine(RestartNextFrame());
+		}
+		else {
+			Destroy(propParentToDestroy);
+		}
+	}
+
+	private IEnumerator RestartNextFrame() {
+		yield return 0;
+		transform.localEulerAngles = new Vector3(0, 0, initEulerAngle);
+		eulerAngleDifference = endEulerAngle - transform.localEulerAngles.z;
+		tweenTimeAux = loopTweenTime;
+        TweenAction();
+	}
+}
