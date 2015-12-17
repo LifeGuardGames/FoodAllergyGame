effa20971acafe8fcdf2f52db59248d238a4af7b
aliken9 <aliken9@gmail.com>
2015-12-17 17:49:49 -0500

    Start integration of growth props

---
+++ FoodAllergyGame/Assets/Scripts/AnimationStartRandomFrame.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationStartRandomFrame : MonoBehaviour {
+
+	public string clipName;
+
+	void Start () {
+		Animation anim = this.GetComponent<Animation>();
+		anim[clipName].time = Random.Range(0.0f, 1.0f);
+	}
+}
