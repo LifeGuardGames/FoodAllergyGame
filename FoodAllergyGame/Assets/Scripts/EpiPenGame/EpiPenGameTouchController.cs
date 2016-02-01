5967fa033284847e8dbc31eccfbab8ca5330cb53
Dylan <dylan@lifeguardgames.com>
2016-02-01 18:01:49 -0500

    Controls WIP for epi pen game

--- FoodAllergyGame/Assets/Scripts/EpiPenGame/EpiPenGameTouchController.cs
+++ FoodAllergyGame/Assets/Scripts/EpiPenGame/EpiPenGameTouchController.cs
@@ -1,15 +1,72 @@
 ﻿using UnityEngine;
+using UnityEngine.EventSystems;
 using System.Collections;
+using UnityEngine.UI;
 
-public class EpiPenGameTouchController : MonoBehaviour {
+public class EpiPenGameTouchController : MonoBehaviour, IBeginDragHandler,IDragHandler, IEndDragHandler{
 
-	// Use this for initialization
-	void Start () {
-	
+	public static GameObject itemBeingDragged;
+	public int order;
+
+	private Transform dragAux;
+	private Transform trashAux;
+	private Vector3 startPosition;
+	private Transform startParent;
+	public Image panelImage;
+
+	public void OnBeginDrag(PointerEventData eventData) {
+		itemBeingDragged = gameObject;
+		startPosition = transform.localPosition;
+		startParent = transform.parent;
+		GetComponent<CanvasGroup>().blocksRaycasts = false;
+
+		itemBeingDragged.transform.SetParent(dragAux);
+		AudioManager.Instance.PlayClip("Button1Down");
+
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	public void OnDrag(PointerEventData data) {
+#if UNITY_EDITOR
+		itemBeingDragged.transform.position = Input.mousePosition;
+#else
+		itemBeingDragged.transform.position = Input.GetTouch(0).position;
+#endif
+	}
+
+
+	public void OnEndDrag(PointerEventData eventData) {
+		itemBeingDragged = null;
+		AudioManager.Instance.PlayClip("Button1Up");
+		GetComponent<CanvasGroup>().blocksRaycasts = true;
+		if(transform.parent == dragAux) {
+			transform.SetParent(startParent);
+			transform.localPosition = startPosition;
+			if(startParent.name == "SelectedGrid") {
+				
+			}
+			else {
+				
+			}
+		}
+		else if(transform.parent == trashAux) {
+			StartCoroutine(DestroySelf());
+		}
+		else {  // Save new parent
+			startParent = transform.parent;
+			startPosition = transform.localPosition;
+			if(startParent.name == "SelectedGrid") {
+				
+			}
+			else {
+				
+			}
+		}
+
+		// Try to hide trash can no matter what
+		MenuManager.Instance.HideTrashCan();
+	}
+	private IEnumerator DestroySelf() {
+		yield return new WaitForEndOfFrame();
+		Destroy(gameObject);
 	}
 }
