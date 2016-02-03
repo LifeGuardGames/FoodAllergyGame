0d7862ac578677f4ad81d64cbd3ce701f3a6e4e2
Dylan <dylan@lifeguardgames.com>
2016-02-03 13:41:45 -0500

    WIP controls for epi pen game

---
+++ FoodAllergyGame/Assets/Scripts/EpiPenGame/EpiPenGameComicSlot.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class EpiPenGameComicSlot : MonoBehaviour, IDropHandler {
+
+	public int slotNumber;
+
+	#region IDropHandler implementation
+	public void OnDrop(PointerEventData eventData) {
+		Debug.Log("rdhbjnfgbkj");
+		EpiPenGamePanel panel = EpiPenGamePanel.itemBeingDragged.GetComponent<EpiPenGamePanel>();
+		EpiPenGameManager.Instance.submittedAnswers.Add(slotNumber, panel.order);
+		panel.gameObject.transform.SetParent(this.transform);
+	}
+	#endregion
+}
