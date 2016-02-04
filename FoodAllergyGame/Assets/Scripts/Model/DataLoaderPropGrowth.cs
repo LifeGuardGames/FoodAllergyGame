9c68d7726422c0262652c0ca9cba9720d5b53bc0
aliken9 <aliken9@gmail.com>
2016-02-04 16:57:17 -0500

    Growth props logic complete

---
+++ FoodAllergyGame/Assets/Scripts/Model/DataLoaderPropGrowth.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DataLoaderPropGrowth : XMLLoaderGeneric<DataLoaderPropGrowth> {
+
+	public static ImmutableDataPropGrowth GetData(string id) {
+		instance.InitXMLLoader();
+		return instance.GetData<ImmutableDataPropGrowth>(id);
+	}
+
+	public static List<ImmutableDataPropGrowth> GetDataList() {
+		instance.InitXMLLoader();
+		return instance.GetDataList<ImmutableDataPropGrowth>();
+	}
+
+	// Given a tier and their propKey, get the data to load
+	public static ImmutableDataPropGrowth GetPropsByTier(int currentTier, string propKey) {
+		List<ImmutableDataPropGrowth> itemList = GetTypeList(propKey);
+		ImmutableDataPropGrowth curr = null;
+		for(int i = 0; i < itemList.Count; i++) {
+			if(itemList[i].Tier <= currentTier) {
+				if(curr == null || curr.Tier < itemList[i].Tier) {
+					curr = itemList[i];
+				}
+			}
+		}
+		return curr;
+	}
+
+	// Filter all grows props by their propKey
+	private static List<ImmutableDataPropGrowth> GetTypeList(string propKey) {
+		List<ImmutableDataPropGrowth> itemList = GetDataList();
+		List<ImmutableDataPropGrowth> currList = new List<ImmutableDataPropGrowth>();
+		for(int i = 0; i < itemList.Count; i++) {
+			if(itemList[i].PropKey == propKey) {
+				currList.Add(itemList[i]);
+			}
+		}
+		return currList;
+	}
+
+	protected override void XMLNodeHandler(string id, IXMLNode xmlNode, Hashtable hashData, string errorMessage) {
+		ImmutableDataPropGrowth data = new ImmutableDataPropGrowth(id, xmlNode, errorMessage);
+		// Store the data
+		if(hashData.ContainsKey(id))
+			Debug.LogError(errorMessage + "Duplicate keys!");
+		else
+			hashData.Add(id, data);
+	}
+
+	protected override void InitXMLLoader() {
+		xmlFileFolderPath = "PropsGrowth";
+	}
+}
---
+++ FoodAllergyGame/Assets/Scripts/Model/ImmutableDataPropGrowth.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class ImmutableDataPropGrowth{
+
+	private string id;
+	public string ID {
+		get { return id; }
+	}
+
+	private int tier;
+	public int Tier {
+		get { return tier; }
+	}
+
+	private string propKey;
+	public string PropKey {
+		get { return propKey; }
+	}
+
+	private string prefabName;
+	public string PrefabName { 
+		get { return prefabName; }
+	}
+
+	public ImmutableDataPropGrowth (string id, IXMLNode xmlNode, string error) {
+		Hashtable hashElements = XMLUtils.GetChildren(xmlNode);
+
+		this.id = id;
+		tier = XMLUtils.GetInt(hashElements["TierUnlock"] as IXMLNode);
+		propKey = XMLUtils.GetString(hashElements["PropKey"] as IXMLNode, null, error);
+		prefabName = XMLUtils.GetString(hashElements["PrefabName"] as IXMLNode, null, error);
+	}
+
+}
