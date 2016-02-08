8a26f55e5d5883d89dddfd3d827cf0917c3decb7
aliken9 <aliken9@gmail.com>
2016-02-08 12:21:04 -0500

    Start integration of EpiPen UI

--- FoodAllergyGame/Assets/Scripts/EpiPenGame/EpiPenGameUiManager.cs
+++ FoodAllergyGame/Assets/Scripts/EpiPenGame/EpiPenGameUiManager.cs
@@ -1,18 +1,7 @@
 ﻿using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
 
-public class EpiPenGameUiManager : MonoBehaviour{
-
-	public List<Transform> pos;
-	public void PlaceInPos(EpiPenGamePanel panel) {
-		foreach(Transform spot in pos) {
-			if(spot.childCount == 0) {
-				panel.transform.SetParent(spot);
-				//panel.transform.position = spot.position;
-				panel.isCorrect = false;
-				break;
-			}
-		}
+public class EpiPenGameUIManager : MonoBehaviour{
+	public void ShowGameOver() {
+		
 	}
 }
