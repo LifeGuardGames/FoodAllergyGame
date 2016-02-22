c0ba02e88ae9015571ee6216d605650e85fadb24
aliken9 <aliken9@gmail.com>
2016-02-22 17:46:30 -0500

    Fixed ParticleUtils scaling issue

--- FoodAllergyGame/Assets/Scripts/Utils And Libraries/ParticleUtils.cs
+++ FoodAllergyGame/Assets/Scripts/Utils And Libraries/ParticleUtils.cs
@@ -5,10 +5,11 @@ public static class ParticleUtils{
 
 	static public void PlayMoneyFloaty(Vector3 pos, int amount){
 		GameObject go = Resources.Load("CoinFloaty") as GameObject;
-		GameObject Instance = GameObject.Instantiate(go, pos, go.transform.rotation) as GameObject;
+		GameObject instance = GameObjectUtils.AddChildGUI(GameObject.Find("Canvas"), go);
+		instance.transform.position = pos;
 
-		Text textScript = Instance.GetComponentInChildren<Text>();
-		Image imageScript = Instance.GetComponentInChildren<Image>();
+		Text textScript = instance.GetComponentInChildren<Text>();
+		Image imageScript = instance.GetComponentInChildren<Image>();
 
 		if(amount > 0){
 			textScript.text = "+" + amount.ToString();
@@ -18,6 +19,13 @@ public static class ParticleUtils{
 			textScript.color = new Color(1f, 0.242f, 0.242f, 1f);
 			imageScript.color = new Color(1f, 0.586f, 0.586f, 1f);
         }
+    }
+
+	static public void PlayHandsFullFloaty(Vector3 pos) {
+		GameObject go = Resources.Load("HandsFullFloaty") as GameObject;
+		GameObject instance = GameObjectUtils.AddChildGUI(GameObject.Find("Canvas"), go);
+		instance.transform.position = pos;
+		AudioManager.Instance.PlayClip("HandsFull");
 	}
 
 	static public void PlayDecoChangePoof(Vector3 pos){
@@ -25,15 +33,9 @@ public static class ParticleUtils{
 		GameObject.Instantiate(go, pos, go.transform.rotation);
 	}
 
-	static public void PlayTeleportParticle(Vector3 pos){
+	static public void PlayTeleportParticle(Vector3 pos) {
 		GameObject go = Resources.Load("ParticleTeleport") as GameObject;
 		GameObject.Instantiate(go, pos, go.transform.rotation);
-	}
-
-	static public void PlayHandsFullFloaty(Vector3 pos){
-		GameObject go = Resources.Load("HandsFullFloaty") as GameObject;
-		GameObject.Instantiate(go, pos, go.transform.rotation);
-		AudioManager.Instance.PlayClip("HandsFull");
 	}
 
 	// When table smasher smashes a table
