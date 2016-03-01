5b7b407539501ea63a6192cfa57653f1a9dd8238
aliken9 <aliken9@gmail.com>
2016-03-01 15:32:55 -0500

    Tier logic fixes

--- FoodAllergyGame/Assets/Scripts/DecoTabController.cs
+++ FoodAllergyGame/Assets/Scripts/DecoTabController.cs
@@ -4,7 +4,6 @@ public class DecoTabController : MonoBehaviour {
 	public DecoTypes deco;
 
 	void Start() {
-		Debug.Log("UNlocked ? " + DecoManager.Instance.IsCategoryUnlocked(deco));
 		if(!DecoManager.Instance.IsCategoryUnlocked(deco)) {
 			gameObject.SetActive(false);
 		}
