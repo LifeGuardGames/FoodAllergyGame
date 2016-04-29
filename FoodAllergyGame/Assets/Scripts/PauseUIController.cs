37de421f8d763530a02f65a69f554c32ba78b625
Dylan <dylan@lifeguardgames.com>
2016-04-29 15:06:30 -0400

    pause fix

--- FoodAllergyGame/Assets/Scripts/PauseUIController.cs
+++ FoodAllergyGame/Assets/Scripts/PauseUIController.cs
@@ -7,12 +7,14 @@ public class PauseUIController : MonoBehaviour {
 	public bool isActive = true;
 
 	public void Show() {
-		if(isActive) { 
+		if(isActive) {
+			TouchManager.Instance.PauseQueue();
 			pauseMenuTween.Show();
 		}
 	}
 
 	public void Hide(){
+		TouchManager.Instance.UnpauseQueue();
 		pauseMenuTween.Hide();
 	}
 
