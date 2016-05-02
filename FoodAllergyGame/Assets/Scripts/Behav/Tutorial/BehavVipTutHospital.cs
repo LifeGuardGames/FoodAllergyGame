c10a85f082be8359f4e6f45d23b5c0165c55e341
Dylan <dylan@lifeguardgames.com>
2016-05-02 17:04:04 -0400

    Vip tutorial fix for shop unlock

---
+++ FoodAllergyGame/Assets/Scripts/Behav/Tutorial/BehavVipTutHospital.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class BehavVipTutHospital : Behav {
+
+	public BehavVipTutHospital() {
+	}
+
+	public override void Reason() {
+	}
+
+	public override void Act() {
+		DataManager.Instance.GameData.Tutorial.IsSpeDecoTutDone = true;
+		RestaurantManager.Instance.sickCustomers.Remove(self.gameObject);
+		RestaurantManager.Instance.GetTable(self.tableNum).inUse = false;
+		DataManager.Instance.GameData.Tutorial.MissedMedic++;
+		if(DataManager.Instance.GameData.Tutorial.MissedMedic >= 3) {
+			DataManager.Instance.GameData.Tutorial.IsMedicTut2Done = false;
+			DataManager.Instance.GameData.Tutorial.MissedMedic = 0;
+		}
+		if(RestaurantManager.Instance.GetTable(self.tableNum).tableType == Table.TableType.VIP) {
+			RestaurantManager.Instance.GetTable(self.tableNum).CustomerLeaving();
+		}
+		// Removes customer and bills the restaurant
+		RestaurantManager.Instance.CustomerLeftFlatCharge(self, Medic.HospitalPrice, true);
+
+		AudioManager.Instance.PlayClip("CustomerDead");
+		self.DestroyOrder();
+		self.DestroySelf(0);
+	}
+}
---
+++ FoodAllergyGame/Assets/Scripts/Behav/Tutorial/BehavVipTutSaved.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class BehavVipTutSaved : Behav {
+
+	public BehavVipTutSaved() {
+
+	}
+
+	public override void Reason() {
+
+	}
+
+	public override void Act() {
+		if(RestaurantManager.Instance.GetTable(self.tableNum).tableType == Table.TableType.VIP) {
+			RestaurantManager.Instance.GetTable(self.tableNum).CustomerLeaving();
+		}
+		DataManager.Instance.GameData.Tutorial.IsSpeDecoTutDone = true;
+		AudioManager.Instance.PlayClip("CustomerSaved");
+		RestaurantManager.Instance.savedCustomers++;
+		self.customerAnim.SetSavedAllergyAttack();
+		RestaurantManager.Instance.sickCustomers.Remove(self.gameObject);
+		self.UpdateSatisfaction(1);
+		self.customerUI.ToggleAllergyAttack(false);
+		self.state = CustomerStates.Saved;
+		self.StopCoroutine("AllergyTimer");
+		RestaurantManager.Instance.GetTable(self.tableNum).inUse = false;
+
+
+		// Removes customer and bills the restaurant
+		RestaurantManager.Instance.CustomerLeftFlatCharge(self, Medic.MedicPrice, true);
+
+		AudioManager.Instance.PlayClip("CustomerLeave");
+		self.DestroySelf(0);
+	}
+}
