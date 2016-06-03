5b76c04d65f4fcd6e67c5c474ed49c031c994ad7
Dylan <dylan@lifeguardgames.com>
2016-06-03 13:17:18 -0400

    amplitude push

--- FoodAllergyGame/Assets/Scripts/DataManager.cs
+++ FoodAllergyGame/Assets/Scripts/DataManager.cs
@@ -76,6 +76,11 @@ public class DataManager : Singleton<DataManager> {
 		//Live token
 		Mixpanel.Token = "4afadc10e61a77c363308040d38be801";
 
+
+		//Amplitude amplitude = Amplitude.Instance;
+		//amplitude.logging = false;
+		//amplitude.init("9196f4d945c306a45a54b9fb8577c017");
+	
 		//Dev Token
 		//Mixpanel.Token = "9b68a5560f74b183728b7c30cb4a54fc";
         Mixpanel.SuperProperties.Add("Days Played", GameData.DayTracker.DaysPlayed);
