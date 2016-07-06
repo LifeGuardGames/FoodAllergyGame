9426efc0b7f880f77b31a0803baf1a6855bfce93
Dylan <dylan@lifeguardgames.com>
2016-07-06 14:34:40 -0400

    juke box prototype an epipen game fix

---
+++ FoodAllergyGame/Assets/Scripts/JukeBox.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JukeBox : Singleton<JukeBox>, IWaiterSelection {
+
+	public GameObject waiterNode;
+
+	public void OnWaiterArrived() {
+		AudioManager.Instance.JukeBox();
+	}
+
+	public void OnClicked() {
+		Waiter.Instance.FindRoute(waiterNode, this);
+	}
+
+	public bool IsQueueable() {
+		return true;
+	}
+
+	public virtual void OnPressAnim() {
+		//animator.SetTrigger("ClickPulse");
+	}
+}
