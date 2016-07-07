ca449f6ae9cb33f2794042f205374ab2ee6bbe89
Dylan <dylan@lifeguardgames.com>
2016-07-07 13:24:02 -0400

    epi pen game swap tokens

--- FoodAllergyGame/Assets/Scripts/EpiPenGame/EpiPenGameSlot.cs
+++ FoodAllergyGame/Assets/Scripts/EpiPenGame/EpiPenGameSlot.cs
@@ -2,7 +2,7 @@
 using UnityEngine.EventSystems;
 
 public class EpiPenGameSlot : MonoBehaviour, IDropHandler {
-	public bool isFinalSlot = true;		//Final slot vs Aux slot
+	public bool isFinalSlot = true;		//Final slot vs Aux slot set to false in editor
 	public int slotNumber;
 
 	public EpiPenGameToken GetToken() {
@@ -31,7 +31,20 @@ public class EpiPenGameSlot : MonoBehaviour, IDropHandler {
 	public void OnDrop(PointerEventData eventData) {
 		if(EpiPenGameToken.itemBeingDragged != null &&
 			((isFinalSlot && transform.childCount == 1) || (!isFinalSlot && transform.childCount == 0))) {
+			SetToken(EpiPenGameToken.itemBeingDragged.GetComponent<EpiPenGameToken>());
+		}
+		else if(EpiPenGameToken.itemBeingDragged != null && (isFinalSlot && transform.childCount > 1)){
+			// Swap token 
+			EpiPenGameToken temp = GetToken();
+            GetToken().transform.SetParent(EpiPenGameToken.itemBeingDragged.GetComponent<EpiPenGameToken>().GetStartPosition());
+			temp.transform.localPosition = Vector3.zero;
+			temp.GetComponent<CanvasGroup>().blocksRaycasts = true;
+			RectTransform rect = temp.GetComponent<RectTransform>();
+			rect.offsetMin = new Vector2(10, 10);
+			rect.offsetMax = new Vector2(-10, -10);
+			EpiPenGameManager.Instance.TokenPlaced();
 
+			//set new token
 			SetToken(EpiPenGameToken.itemBeingDragged.GetComponent<EpiPenGameToken>());
 		}
 	}
